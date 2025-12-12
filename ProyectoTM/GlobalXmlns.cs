a05720ddcc27545ed4715da947075bfbf8de3707
Fernando-Albarran <falbarranco.inf@upsa.es>
2025-12-13 00:04:28 +0100

    Proyecto

---
+++ ProyectoTM/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ProyectoTM")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ProyectoTM.Pages")]
